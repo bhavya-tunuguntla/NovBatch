026d4152d43de6ea8154ff5d29375c62019ab20d
Prasanna <prasanna@hexaware.com>
2020-11-23 11:05:37 +0530

    code

---
+++ Practice1.sql
@@ -0,0 +1,30 @@
+abs() -> returns the absolute value 
+
+select abs(-12);
+
+power(n,m) : returns n power m value 
+
+select power(2,3);
+
+sqrt(n) : Returns sqrt. value 
+
+select sqrt(49);
+
+ceiling() : returns the greatest integer value 
+
+select ceiling(12.0000001);
+
+floor() : Returns the smallest integer value 
+
+select floor(12.9999999);
+
+select round(8767.8878,3);
+select round(8767.8878,2);
+select round(8767.8878,1);
+select round(8767.8878,0);
+select round(8767.8878,-1);
+select round(8767.8878,-2);
+select round(8767.8878,-3);
+
+
+
